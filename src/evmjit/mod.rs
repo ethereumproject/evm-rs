e687c2cae439f1838b4266b740c534cae94c5f64
Michael Collison <collison956@gmail.com>
2019-04-01 21:21:03 -0700

    New method is_ptr_to_int256

--- src/evmjit/mod.rs
+++ src/evmjit/mod.rs
@@ -93,6 +93,7 @@ pub trait BasicTypeEnumCompare {
     fn is_int256(self) -> bool;
     fn is_ptr_type(self) -> bool;
     fn is_ptr_to_int8(self) -> bool;
+    fn is_ptr_to_int256(self) -> bool;
     fn is_ptr_to_struct(self) -> bool;
     fn is_array_t(self) -> bool;
     fn is_array_of_len_n(self, len:u32) -> bool;
@@ -140,6 +141,15 @@ impl BasicTypeEnumCompare for BasicTypeEnum {
 
         let elem_t = self.as_pointer_type().get_element_type();
         elem_t.is_int_type() && (elem_t.as_int_type().get_bit_width() == 8)
+    }
+
+    fn is_ptr_to_int256(self) -> bool {
+        if !self.is_ptr_type() {
+            false;
+        }
+
+        let elem_t = self.as_pointer_type().get_element_type();
+        elem_t.is_int_type() && (elem_t.as_int_type().get_bit_width() == 256)
     }
 
     fn is_ptr_to_struct(self) -> bool {
