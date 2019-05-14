47eb168080282ac3b80226220d1ff1c1f8855d2c
Michael Collison <collison956@gmail.com>
2019-05-13 18:39:20 -0700

    Add signed 256-bit divide and unit tests

--- src/evmjit/compiler/runtime_functions/divmod.rs
+++ src/evmjit/compiler/runtime_functions/divmod.rs
@@ -144,11 +144,12 @@ impl<'a> DivModDeclarationManager<'a> {
 
         let undef_ret_val = ret_type.get_undef();
         let mut ret = temp_builder.build_insert_element(undef_ret_val, q_ret_val, zero_64_t, "ret0");
-        ret = temp_builder.build_insert_element(ret.into_vector_value(), r_ret.as_basic_value().into_int_value(), one_64_t, "ret");
+        ret = temp_builder.build_insert_element(ret, r_ret.as_basic_value().into_int_value(), one_64_t, "ret");
         temp_builder.build_return(Some(&ret));
 
         divmod_func
     }
+
 
     pub fn create_udivmod256_func(&self) -> FunctionValue {
         let func_name = "jit.udivmod.256";
@@ -181,8 +182,7 @@ impl<'a> DivModDeclarationManager<'a> {
 
         if udiv256_func_found.is_some() {
             udiv256_func_found.unwrap()
-        }
-        else {
+        } else {
             let context = self.m_context.llvm_context();
             let module = self.m_context.module();
             let attr_factory = self.m_context.attributes();
@@ -191,7 +191,7 @@ impl<'a> DivModDeclarationManager<'a> {
             let ret_type = word_type;
 
             let udiv_func_type = ret_type.fn_type(&[word_type.into(), word_type.into()], false);
-            let udiv256_func = module.add_function (func_name, udiv_func_type, Some(Private));
+            let udiv256_func = module.add_function(func_name, udiv_func_type, Some(Private));
 
             // Function does not throw
             udiv256_func.add_attribute(0, *attr_factory.attr_nounwind());
@@ -227,6 +227,131 @@ impl<'a> DivModDeclarationManager<'a> {
             udiv256_func
         }
     }
+
+    fn create_sdivrem256_func(&self) -> FunctionValue {
+        let func_name = "jit.sdivrem.256";
+        let sdivrem256_func_found = self.m_context.module().get_function(func_name);
+
+        if sdivrem256_func_found.is_some() {
+            sdivrem256_func_found.unwrap()
+        } else {
+            let context = self.m_context.llvm_context();
+            let module = self.m_context.module();
+            let attr_factory = self.m_context.attributes();
+            let udivrem_func = self.create_udivmod256_func();
+
+            let word_type = self.m_context.evm_types().get_word_type();
+            let ret_type = word_type.vec_type(2);;
+
+            let sdivrem256_func_type = ret_type.fn_type(&[word_type.into(), word_type.into()], false);
+            let sdivrem256_func = module.add_function(func_name, sdivrem256_func_type, Some(Private));
+
+            // Function does not throw
+            sdivrem256_func.add_attribute(0, *attr_factory.attr_nounwind());
+
+            // Function does not access memory
+            sdivrem256_func.add_attribute(0, *attr_factory.attr_readnone());
+
+            // Give the function parameters names
+            assert!(sdivrem256_func.get_nth_param(0).is_some());
+            let x = sdivrem256_func.get_nth_param(0).unwrap();
+            x.into_int_value().set_name("dividend");
+            let x_val = x.into_int_value();
+
+            assert!(sdivrem256_func.get_nth_param(1).is_some());
+            let y = sdivrem256_func.get_nth_param(1).unwrap();
+            y.into_int_value().set_name("divisor");
+            let y_val = y.into_int_value();
+
+            let temp_builder = context.create_builder();
+            let entry_bb = context.append_basic_block(&sdivrem256_func, "sdivrem256_entry");
+            temp_builder.position_at_end(&entry_bb);
+
+            let zero = word_type.const_zero();
+            let x_is_neg = temp_builder.build_int_compare(IntPredicate::SLT, x_val, zero, "");
+            let x_neg = temp_builder.build_int_sub(zero, x_val, "");
+            let x_abs = temp_builder.build_select(x_is_neg, x_neg, x_val, "");
+
+            let y_is_neg = temp_builder.build_int_compare(IntPredicate::SLT, y_val, zero, "");
+            let y_neg = temp_builder.build_int_sub(zero, y_val, "");
+            let y_abs = temp_builder.build_select(y_is_neg, y_neg, y_val, "");
+
+            let zero_64_t = context.i64_type().const_zero();
+            let one_64_t = context.i64_type().const_int(1, false);
+
+            let udivrem_result = temp_builder.build_call(udivrem_func, &[x_abs.into(), y_abs.into()], "");
+            let udivrem_val = udivrem_result.try_as_basic_value().left().unwrap().into_vector_value();
+            let q_abs = temp_builder.build_extract_element(udivrem_val, zero_64_t, "");
+            let r_abs = temp_builder.build_extract_element(udivrem_val, one_64_t, "");
+
+            // the remainder has the same sign as dividend
+
+            let r_neg = temp_builder.build_int_sub(zero, r_abs.into_int_value(), "");
+            let rem = temp_builder.build_select(x_is_neg, r_neg, r_abs.into_int_value(), "");
+
+            let q_neg = temp_builder.build_int_sub(zero, q_abs.into_int_value(), "");
+            let xy_opposite = temp_builder.build_xor(x_is_neg, y_is_neg, "");
+            let quot = temp_builder.build_select(xy_opposite, q_neg, q_abs.into_int_value(), "");
+
+            let undef_ret_val = ret_type.get_undef();
+            let mut ret = temp_builder.build_insert_element(undef_ret_val, quot.into_int_value(), zero_64_t, "");
+            ret = temp_builder.build_insert_element(ret, rem.into_int_value(), one_64_t, "");
+            temp_builder.build_return(Some(&ret));
+
+            sdivrem256_func
+        }
+    }
+
+    pub fn create_sdiv256_func(&self) -> FunctionValue {
+        let func_name = "jit.sdiv.256";
+        let sdiv256_func_found = self.m_context.module().get_function(func_name);
+
+        if sdiv256_func_found.is_some() {
+            sdiv256_func_found.unwrap()
+        } else {
+            let context = self.m_context.llvm_context();
+            let module = self.m_context.module();
+            let attr_factory = self.m_context.attributes();
+            let sdivrem_func = self.create_sdivrem256_func();
+            let word_type = self.m_context.evm_types().get_word_type();
+            let ret_type = word_type;
+
+            let sdiv_func_type = ret_type.fn_type(&[word_type.into(), word_type.into()], false);
+            let sdiv256_func = module.add_function(func_name, sdiv_func_type, Some(Private));
+
+            // Function does not throw
+            sdiv256_func.add_attribute(0, *attr_factory.attr_nounwind());
+
+            // Function does not access memory
+            sdiv256_func.add_attribute(0, *attr_factory.attr_readnone());
+
+            // Give the function parameters names
+            assert!(sdiv256_func.get_nth_param(0).is_some());
+            let dividend = sdiv256_func.get_nth_param(0).unwrap();
+            dividend.into_int_value().set_name("dividend");
+            let dividend_val = dividend.into_int_value();
+
+            assert!(sdiv256_func.get_nth_param(1).is_some());
+            let divisor = sdiv256_func.get_nth_param(1).unwrap();
+            divisor.into_int_value().set_name("divisor");
+            let divisor_val = divisor.into_int_value();
+
+            let temp_builder = context.create_builder();
+            let entry_bb = context.append_basic_block(&sdiv256_func, "sdiv256_entry");
+            temp_builder.position_at_end(&entry_bb);
+
+            // All we need to do is call the sdivrem function and extract the division result from element 0
+
+            let sdivrem_result = temp_builder.build_call(sdivrem_func, &[dividend_val.into(), divisor_val.into()], "");
+            let sdivrem_val = sdivrem_result.try_as_basic_value().left().unwrap().into_vector_value();
+
+            let index = context.i64_type().const_zero();
+            let sdiv_result = temp_builder.build_extract_element(sdivrem_val, index, "");
+
+            temp_builder.build_return(Some(&sdiv_result));
+            sdiv256_func
+        }
+    }
 }
 
 #[cfg(test)]
@@ -237,6 +362,7 @@ mod divmod_test {
     use inkwell::execution_engine::{ExecutionEngine, FunctionLookupError, JitFunction};
     use inkwell::OptimizationLevel;
     type DivTestFunc = unsafe extern "C" fn(u64, u64) -> u64;
+    type SDivTestFunc = unsafe extern "C" fn(i64, i64) -> i64;
 
     fn jit_compile_udiv256_test(
         jitctx: &JITContext,
@@ -272,6 +398,40 @@ mod divmod_test {
         unsafe { execution_engine.get_function::<DivTestFunc>("udiv256_test") }
     }
 
+    fn jit_compile_sdiv256_test(
+        jitctx: &JITContext,
+        execution_engine: &ExecutionEngine,
+    ) -> Result<JitFunction<SDivTestFunc>, FunctionLookupError> {
+        let module = jitctx.module();
+        let builder = jitctx.builder();
+        let context = jitctx.llvm_context();
+        let types_instance = jitctx.evm_types();
+        let word_type = types_instance.get_word_type();
+        let i64_type = context.i64_type();
+        let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into()], false);
+
+        let function = module.add_function("sdiv256_test", fn_type, None);
+        let basic_block = context.append_basic_block(&function, "entry");
+
+        builder.position_at_end(&basic_block);
+
+        let x = function.get_nth_param(0).unwrap().into_int_value();
+        let x_256_val = builder.build_int_s_extend(x, word_type, "x.256");
+
+        let y = function.get_nth_param(1).unwrap().into_int_value();
+        let y_256_val = builder.build_int_s_extend(y, word_type, "x.256");
+
+        let div_decl = DivModDeclarationManager::new(jitctx);
+
+        let sdiv_256_func = div_decl.create_sdiv256_func();
+        let sdiv256result = builder.build_call(sdiv_256_func, &[x_256_val.into(), y_256_val.into()], "");
+        let ret_val_256 = sdiv256result.try_as_basic_value().left().unwrap().into_int_value();
+        let ret_val_64 = builder.build_int_truncate(ret_val_256, i64_type, "");
+        builder.build_return(Some(&ret_val_64));
+
+        unsafe { execution_engine.get_function::<SDivTestFunc>("sdiv256_test") }
+    }
+
     #[test]
     fn test_udiv256() {
         let jitctx = JITContext::new();
@@ -283,33 +443,17 @@ mod divmod_test {
 
         module.print_to_stderr();
 
+
         let mut rng = rand::thread_rng();
         let divisor_range = Uniform::from(1..std::u64::MAX);
 
-        let mut divisor : u64 = 1;
-        let mut dividend: u64 = 1;
-        let mut quotient : u64 = 1;
+        let mut divisor : u64;
+        let mut dividend: u64;
+        let mut quotient : u64;
 
-        let mut number = 0;
-
-        while number != 20 {
-            println!("{}!", number);
+        for _ in 1..100  {
             divisor = divisor_range.sample(&mut rng);
             dividend = rng.gen_range(divisor, std::u64::MAX);
-            println!("Test number = {:?}", number);
-            println!("Random Divisor = {:?}", divisor);
-            println!("Random Dividend = {:?}", dividend);
-            quotient  = dividend/divisor;
-            unsafe {
-                assert_eq!(udiv_jit_func.call(dividend, divisor), quotient);
-            }
-            number = number + 1;
-        }
-
-        for x in 1..20 {
-            divisor = divisor_range.sample(&mut rng);
-            dividend = rng.gen_range(divisor, std::u64::MAX);
-            println!("Test number = {:?}", x);
             println!("Random Divisor = {:?}", divisor);
             println!("Random Dividend = {:?}", dividend);
             quotient  = dividend/divisor;
@@ -317,6 +461,7 @@ mod divmod_test {
                 assert_eq!(udiv_jit_func.call(dividend, divisor), quotient);
             }
         }
+
 
         let x = 200u64;
         let y = 3u64;
@@ -326,6 +471,46 @@ mod divmod_test {
 
         unsafe {
             assert_eq!(udiv_jit_func.call(x, y), x/y);
+        }
+    }
+
+    #[test]
+    fn test_sdiv256() {
+        let jitctx = JITContext::new();
+        let module = jitctx.module();
+
+        let execution_engine = module.create_jit_execution_engine(OptimizationLevel::Default).unwrap();
+
+        let sdiv_jit_func = jit_compile_sdiv256_test(&jitctx, &execution_engine).unwrap();
+
+        module.print_to_stderr();
+
+
+        let mut rng = rand::thread_rng();
+        let divisor_range = Uniform::from(std::i64::MIN..std::i64::MAX);
+
+        let mut divisor : i64;
+        let mut dividend: i64;
+        let mut quotient : i64;
+
+
+        for _ in 1..100 {
+            divisor = divisor_range.sample(&mut rng);
+            dividend = rng.gen_range(divisor, std::i64::MAX);
+            println!("Random Divisor = {:?}", divisor);
+            println!("Random Dividend = {:?}", dividend);
+            quotient  = dividend/divisor;
+            unsafe {
+                assert_eq!(sdiv_jit_func.call(dividend, divisor), quotient);
+            }
+        }
+
+
+        let x = -200i64;
+        let y = 3i64;
+
+        unsafe {
+            assert_eq!(sdiv_jit_func.call(x, y), x/y);
         }
     }
 }
